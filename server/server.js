5da0e6c77114bf24816a89de4bcf7f4fc14deab5
light <light2adrifter@yahoo.com>
2022-04-03 00:11:09 -0400

    last attempt before bed, changing start:dev for herokus sake

--- server/server.js
+++ server/server.js
@@ -5,8 +5,6 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
 
-require('dotenv').config();
-
 
 const app = express();
 const PORT = process.env.PORT || 8080;
