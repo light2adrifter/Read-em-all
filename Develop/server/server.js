01c8193cfcd5c18f803d1e8fbccc20a646becb71
light <light2adrifter@yahoo.com>
2022-04-02 21:33:18 -0400

    removed uneeded files/folders after adding mutations/queries/typeDefs and editing original files. May have npm i some of the programs in wrong folder, so package.json is irregular. Going to do battle with heroku.

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.applyMiddleware({ app });
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -14,8 +23,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res)=> {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+});
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Using GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
 });
