62ec50677bc6858b1221b451bd578850bcaebf59
light <light2adrifter@yahoo.com>
2022-04-03 10:51:58 -0400

    trying moar thing

--- server/server.js
+++ server/server.js
@@ -28,6 +28,8 @@ app.get('*', (req, res)=> {
 });
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`Using GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+  app.listen(PORT, () => { 
+      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
